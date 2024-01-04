ff2fbac82e8b0cd1ccca2180dc063caaa9b58f0a
Akimthedream2014 <kens2965@gmail.com>
2024-01-04 10:39:28 -0800

    files

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,4 +3,4 @@ def print_last_digit(number):
     last_digit = str(number)[-1]
     last_digit = int(last_digit)
     print(f"{last_digit:d}", end="")
-    return (last_digit
+    return (last_digit)
